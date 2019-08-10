ed46a0ec476c6e5ecfcdd6d63abc2f13a4149a9f
Amanda Filizola <amandafilizola@polijuniorengenharia.com.br>
2019-08-10 16:11:36 -0300

    adicionando o banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './frame/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { BannerComponent } from './shared/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
