e707d69b988d7a15a3bf92aa83bfb45adf944a0c
Amanda Filizola <amandafilizola@polijuniorengenharia.com.br>
2019-08-11 00:03:02 -0300

    terminando quem somos e serviços

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './frame/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { BannerComponent } from './shared/banner/banner.component';
+import { QuemsomosComponent } from './shared/quemsomos/quemsomos.component';
+import { ServicosComponent } from './shared/servicos/servicos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    QuemsomosComponent,
+    ServicosComponent,
   ],
   imports: [
     BrowserModule
